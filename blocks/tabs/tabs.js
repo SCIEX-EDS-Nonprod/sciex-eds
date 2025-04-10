f44cffb29a62ce7e5c3566dad36f809aef7824e6
Venkatalakshmi-nelluri <142808398+Venkatalakshmi-nelluri@users.noreply.github.com>
2025-04-10 16:17:16 +0530

    Tabs block js change

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,5 +1,5 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
+import { } from '../../scripts/aem.js';
+import { } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   /* change to ul, li */
@@ -7,13 +7,13 @@ export default function decorate(block) {
   const blockDiv = document.createElement('div');
   blockDiv.classList.add('tabs-nav', 'tab-buttons');
   [...block.children].forEach((row) => {
-   console.log('lenght>'+row.children.length);
+    console.log(`lenght>${row.children.length}`);
     const tabDIv = document.createElement('div');
-    if(row.children.length == 2){
+    if (row.children.length === 2) {
       tabDIv.id = row.children[1].textContent;
       tabDIv.classList.add('tab-section');
       tabDIv.textContent = row.children[0].textContent;
-  }
+    }
     blockDiv.append(tabDIv);
   });
   block.textContent = '';
