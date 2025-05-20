26739d523f5779d97f6a4b020822aaaf56324fd2
subrahmanyam <subrahmanyam.y@gspann.com>
2025-05-20 16:13:51 +0530

    Sciex text Id added

--- blocks/sciex-text/sciex-text.js
+++ blocks/sciex-text/sciex-text.js
@@ -1,15 +1,22 @@
 import { } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
-  let alignment = block.children[0].textContent;
-  if (alignment && alignment.trim() !== '') {
-    alignment = `text-${alignment.trim()}`;
-  } else {
-    alignment = 'text-left';
-  }
-  const content = block.children[1];
-  content.className = alignment;
+   const blockId = block.children[0].textContent;
+    if (blockId && blockId.trim() !== '') {
+        blockId = blockId.trim();
+    } else {
+        blockId = 'abstract';
+    }
+    let alignment = block.children[1].textContent;
+    if (alignment && alignment.trim() !== '') {
+    alignment = alignment.trim();
+    } else {
+    alignment = "text-left"
+    }
+    const content = block.children[2];
+    content.id = blockId;
+    content.className = alignment;
 
-  block.textContent = '';
-  block.append(content);
+    block.textContent = '';
+    block.append(content);
 }
