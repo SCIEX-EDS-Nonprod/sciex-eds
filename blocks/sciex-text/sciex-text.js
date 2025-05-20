0558253a703db93a0fffa5648862da1039d97657
subrahmanyam <subrahmanyam.y@gspann.com>
2025-05-20 16:17:05 +0530

    Lint issue fix chages

--- blocks/sciex-text/sciex-text.js
+++ blocks/sciex-text/sciex-text.js
@@ -1,22 +1,22 @@
 import { } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
-   const blockId = block.children[0].textContent;
-    if (blockId && blockId.trim() !== '') {
-        blockId = blockId.trim();
-    } else {
-        blockId = 'abstract';
-    }
-    let alignment = block.children[1].textContent;
-    if (alignment && alignment.trim() !== '') {
+  let blockId = block.children[0].textContent;
+  if (blockId && blockId.trim() !== '') {
+    blockId = blockId.trim();
+  } else {
+    blockId = 'abstract';
+  }
+  let alignment = block.children[1].textContent;
+  if (alignment && alignment.trim() !== '') {
     alignment = alignment.trim();
-    } else {
-    alignment = "text-left"
-    }
-    const content = block.children[2];
-    content.id = blockId;
-    content.className = alignment;
+  } else {
+    alignment = 'text-left';
+  }
+  const content = block.children[2];
+  content.id = blockId;
+  content.className = alignment;
 
-    block.textContent = '';
-    block.append(content);
+  block.textContent = '';
+  block.append(content);
 }
