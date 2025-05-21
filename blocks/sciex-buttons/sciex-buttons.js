6333196322f602273de8ce943440b0c3580bd03e
nikhilsciex <nikhil.lale1@sciex.com>
2025-05-21 17:24:04 +0530

    Implemtnt nested button properties.

--- blocks/sciex-buttons/sciex-buttons.js
+++ blocks/sciex-buttons/sciex-buttons.js
@@ -6,46 +6,41 @@ export default async function decorate(block) {
 
   const fragment = document.createDocumentFragment();
 
-  // Create a container for the decorated buttons
   const wrapper = document.createElement('div');
   wrapper.classList.add('button-block', `align-${alignment}`);
 
   rows.slice(1).forEach((row) => {
-    const cells = Array.from(row.querySelectorAll('p')).map((p) => p.textContent.trim());
-    console.log("cells", cells);
-
+    const paragraphs = Array.from(row.querySelectorAll('p'));
+    const picture = row.querySelector('picture');
+  
+    const cells = paragraphs.map((p) => p.textContent.trim());
+  
     const [
       type = 'primary',
       text = '',
       link = '#',
       showSvgRaw = 'false',
-      icon = '',
+      ,
       target = '_self',
     ] = cells;
-
+  
     const showSvg = showSvgRaw.toLowerCase() === 'true';
-
+  
     const button = document.createElement('a');
     button.href = link;
     button.target = target;
     button.className = `button ${type}`;
     button.textContent = text;
-
-    if (showSvg && icon) {
-      console.log("iconPath", icon)
-      // const cleanSvg = icon.replace(/^["'`]+|["'`]+$/g, '');
-      // const parsedSvgDoc = new DOMParser().parseFromString(cleanSvg, 'image/svg+xml');
-      const parsedSvg =  document.createElement('img');
-      parsedSvg.src = icon;
-      parsedSvg.alt = 'icon';
-      if (parsedSvg) {
-        parsedSvg.classList.add('inline-icon');
-        button.appendChild(parsedSvg);
-      }
+  
+    if (showSvg && picture) {
+      const clonedPicture = picture.cloneNode(true);
+      clonedPicture.classList.add('inline-icon');
+      button.appendChild(clonedPicture);
     }
-
+  
     wrapper.appendChild(button);
   });
+  
 
   fragment.appendChild(wrapper);
 
