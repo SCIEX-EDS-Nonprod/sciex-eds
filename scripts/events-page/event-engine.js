c3ea23750ef639726e65a2634faf7bef12a535d5
nikhilsciex <nikhil.lale1@sciex.com>
2025-05-22 17:20:27 +0530

    Fetch dynamic orgId and access token.

--- scripts/events-page/event-engine.js
+++ scripts/events-page/event-engine.js
@@ -1,19 +1,19 @@
 /* eslint-disable */
 import { buildSearchEngine } from 'https://static.cloud.coveo.com/headless/v3/headless.esm.js';
 
-let accessToken = 'xx41750787-38bf-4ef5-b6ed-de7c116eae56';
-let organizationId = 'danahernonproduction1892f3fhz';
+let accessToken = '';
+let organizationId = '';
 
-// let mainDiv = document.querySelector('main');
-// const sections = mainDiv.querySelector('.customer-document').children;
-// Array.from(sections).forEach((section, index) => {
-//   const iteration = index + 1;
-//   if(iteration === 5){
-//     organizationId = section.querySelector('div').innerText;
-//   } else if(iteration === 6){
-//     accessToken = section.querySelector('div').innerText;
-//   }
-// });
+let mainDiv = document.querySelector('main');
+const sections = mainDiv.querySelector('.events').children;
+Array.from(sections).forEach((section, index) => {
+  const iteration = index + 1;
+  if(iteration === 2){
+    organizationId = section.querySelector('div').innerText;
+  } else if(iteration === 3){
+    accessToken = section.querySelector('div').innerText;
+  }
+});
 
 export const eventSearchEngine = buildSearchEngine({
   configuration: {
