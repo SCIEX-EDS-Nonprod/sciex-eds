e6d4f5b9ef28eee0af8849d63c81d5f3a69e30be
nikhilsciex <nikhil.lale1@sciex.com>
2025-06-09 17:29:48 +0530

    PR For reverted changes

--- scripts/translation.js
+++ scripts/translation.js
@@ -13,7 +13,6 @@ export const i18n = {
     searchTerm: 'Search term',
     result: 'Results',
     of: 'of',
-    querySummary: 'Results 1-10 of 1201',
     sortBy: 'Sort By',
     relevancy: 'Relevancy',
     title: 'Title',
@@ -29,6 +28,7 @@ export const i18n = {
     assetType: 'Asset type',
     language: 'Language',
     courseType: 'Course type',
+    courseLevel: 'Course level',
     trainingTopic: 'Training topic',
     techniques: 'Techniques',
     trainingType: 'Training type',
@@ -49,6 +49,9 @@ export const i18n = {
     showMore: 'Show More',
     showLess: 'Show Less',
     filter: 'Filter',
+    partNumber: 'Part Number',
+    lotNumber: 'Lot Number',
+    kitNumber: 'Kit Number'
   },
   "zh-cn": {
     contentType: '内容类别',
@@ -63,7 +66,6 @@ export const i18n = {
     searchTerm: '搜索关键词',
     result: '结果数',
     of: '总数',
-    querySummary: '结果数1-10，总数1201',
     sortBy: '排序方式',
     relevancy: '相关性',
     title: '标题',
@@ -79,6 +81,7 @@ export const i18n = {
     assetType: '内容类别',
     language: '语言',
     courseType: '课程目录',
+    courseLevel: '课程级别',
     trainingTopic: '培训主题',
     techniques: '技术',
     trainingType: '培训类别',
@@ -99,6 +102,9 @@ export const i18n = {
     showMore: '显示更多',
     showLess: '显示较少',
     filter: '筛选',
+    partNumber: '零件编号',
+    lotNumber: '批号',
+    kitNumber: '套件编号'
   },
   "ja": {
     contentType: 'コンテンツタイプ',
@@ -113,7 +119,6 @@ export const i18n = {
     searchTerm: '検索語',
     result: '検索結果',
     of: 'of',
-    querySummary: 'Results 1-10 of 1201',
     sortBy: '並べ替え',
     relevancy: '関連性',
     title: 'タイトル',
@@ -129,6 +134,7 @@ export const i18n = {
     assetType: 'アセットタイプ',
     language: '言語',
     courseType: 'コースタイプ',
+    courseLevel: 'コースレベル',
     trainingTopic: 'トレーニングトピック',
     techniques: '技術',
     trainingType: 'トレーニングタイプ',
@@ -149,5 +155,8 @@ export const i18n = {
     showMore: 'もっと見る',
     showLess: '表示を減らす',
     filter: 'フィルター',
+    partNumber: '部品番号',
+    lotNumber: 'ロット番号',
+    kitNumber: 'キット番号'
   },
 };
