d86ea896d7be6e881180b6c760f77751493ae6b8
Venkatalakshmi-nelluri <142808398+Venkatalakshmi-nelluri@users.noreply.github.com>
2025-10-16 17:28:09 +0530

    Text Container block

--- blocks/text-container/text-container.js
+++ blocks/text-container/text-container.js
@@ -3,5 +3,8 @@ import {} from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   const ul = document.createElement('ul');
+  block.forEach((row, index) => {
+    console.log(index+">"+row.outerHTML);
+  });
   block.append(ul);
 }
