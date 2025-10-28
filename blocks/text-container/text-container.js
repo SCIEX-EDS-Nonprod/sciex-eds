468d07c432b752d408790e94e96ffeeca0157f2f
Venkatalakshmi-nelluri <142808398+Venkatalakshmi-nelluri@users.noreply.github.com>
2025-10-28 19:25:49 +0530

    Text Container issue

--- blocks/text-container/text-container.js
+++ blocks/text-container/text-container.js
@@ -2,9 +2,5 @@ import {} from '../../scripts/aem.js';
 import {} from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  const ul = document.createElement('ul');
-  console.log('11');
-  block.innerHTML = '';
-  block.append(ul);
-  console.log('12');
+  console.log('12>'+block.outerHTML);
 }
