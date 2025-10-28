fbcbf72f9917c90b6560044616970cdaf934282a
Venkatalakshmi-nelluri <142808398+Venkatalakshmi-nelluri@users.noreply.github.com>
2025-10-28 19:36:21 +0530

    Text Container issue

--- blocks/text-container/text-container.js
+++ blocks/text-container/text-container.js
@@ -1,23 +1,19 @@
 import {} from '../../scripts/aem.js';
-import {moveInstrumentation} from '../../scripts/scripts.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row, index) => {
     const li = document.createElement('li');
     if (index === 1 && row.textContent.includes('2')) {
-      console.log('1');
       ul.classList.add('text-container-header', 'text-container-columns');
     } else if (index === 1 && row.textContent.includes('1')) {
-      console.log('2');
       ul.classList.remove('text-container-header', 'text-container-columns');
     } else if (index > 1) {
-      console.log('3');
       const children = [...row.children];
 
       const column = document.createElement('div');
       column.className = 'text-container-column';
-      console.log('4');
       // Title
       const title = children[0]?.textContent?.trim();
       if (title) {
@@ -26,7 +22,6 @@ export default function decorate(block) {
         h2.textContent = title;
         column.appendChild(h2);
       }
-      console.log('5');
       // Sub Heading
       const subheading = children[1]?.textContent?.trim();
       if (subheading) {
@@ -35,7 +30,6 @@ export default function decorate(block) {
         p.textContent = subheading;
         column.appendChild(p);
       }
-      console.log('6');
       // Description
       const desc = children[3]?.textContent?.trim();
       if (desc) {
@@ -44,7 +38,6 @@ export default function decorate(block) {
         p.textContent = desc;
         column.appendChild(p);
       }
-      console.log('7');
       const buttonGroup = document.createElement('div');
       buttonGroup.className = 'button-block';
       for (let i = 5; i + 3 < children.length; i += 4) {
@@ -52,7 +45,6 @@ export default function decorate(block) {
         const alt = children[i + 1]?.textContent?.trim();
         const link = children[i + 2]?.querySelector('a');
         const target = children[i + 3]?.textContent?.trim();
-        console.log('8');
         if (link && label) {
           const a = document.createElement('a');
           a.href = link.getAttribute('href') || '#';
@@ -70,7 +62,6 @@ export default function decorate(block) {
             span.className = 'icon';
             a.appendChild(span);
           }
-          console.log('9');
           if (target) a.setAttribute('target', target);
 
           const wrapper = document.createElement('div');
@@ -80,17 +71,14 @@ export default function decorate(block) {
           buttonGroup.appendChild(wrapper);
         }
       }
-      console.log('10');
       if (buttonGroup.children.length > 0) {
         column.appendChild(buttonGroup);
       }
       li.append(column);
-       moveInstrumentation(row, li);
+      moveInstrumentation(row, li);
       ul.append(li);
     }
   });
-  console.log('11');
   block.textContent = '';
   block.append(ul);
-  console.log('12');
 }
