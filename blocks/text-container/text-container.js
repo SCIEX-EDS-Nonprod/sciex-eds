cf3ca5b7e8ca2bf50fbe04046a84414b6b322a50
Venkatalakshmi-nelluri <142808398+Venkatalakshmi-nelluri@users.noreply.github.com>
2025-10-28 18:54:32 +0530

    Text Container issue

--- blocks/text-container/text-container.js
+++ blocks/text-container/text-container.js
@@ -1,4 +1,3 @@
-/content/SCIEX/language/masters/en/tech-notes/biopharma/comprehensive-workflow-of-intact-and-subunit-mass-analysis-for-monoclonal-antibodies-mab
 import {} from '../../scripts/aem.js';
 import {} from '../../scripts/scripts.js';
 
@@ -7,18 +6,18 @@ export default function decorate(block) {
   [...block.children].forEach((row, index) => {
     const li = document.createElement('li');
     if (index === 1 && row.textContent.includes('2')) {
-      console.log("1");
+      console.log('1');
       ul.classList.add('text-container-header', 'text-container-columns');
     } else if (index === 1 && row.textContent.includes('1')) {
-      console.log("2")
+      console.log('2');
       ul.classList.remove('text-container-header', 'text-container-columns');
     } else if (index > 1) {
-      console.log("3")
+      console.log('3');
       const children = [...row.children];
 
       const column = document.createElement('div');
       column.className = 'text-container-column';
-console.log("4")
+      console.log('4');
       // Title
       const title = children[0]?.textContent?.trim();
       if (title) {
@@ -27,7 +26,7 @@ console.log("4")
         h2.textContent = title;
         column.appendChild(h2);
       }
-      console.log("5")
+      console.log('5');
       // Sub Heading
       const subheading = children[1]?.textContent?.trim();
       if (subheading) {
@@ -36,7 +35,7 @@ console.log("4")
         p.textContent = subheading;
         column.appendChild(p);
       }
-console.log("6")
+      console.log('6');
       // Description
       const desc = children[3]?.textContent?.trim();
       if (desc) {
@@ -45,7 +44,7 @@ console.log("6")
         p.textContent = desc;
         column.appendChild(p);
       }
-      console.log("7")
+      console.log('7');
       const buttonGroup = document.createElement('div');
       buttonGroup.className = 'button-block';
       for (let i = 5; i + 3 < children.length; i += 4) {
@@ -53,7 +52,7 @@ console.log("6")
         const alt = children[i + 1]?.textContent?.trim();
         const link = children[i + 2]?.querySelector('a');
         const target = children[i + 3]?.textContent?.trim();
-console.log("8")
+        console.log('8');
         if (link && label) {
           const a = document.createElement('a');
           a.href = link.getAttribute('href') || '#';
@@ -67,11 +66,11 @@ console.log("8")
           } else if (i === 13) {
             a.className = ' link';
             const span = document.createElement('span');
-            span.id="right-arrow";
+            span.id = 'right-arrow';
             span.className = 'icon';
             a.appendChild(span);
           }
-          console.log("9")
+          console.log('9');
           if (target) a.setAttribute('target', target);
 
           const wrapper = document.createElement('div');
@@ -81,7 +80,7 @@ console.log("8")
           buttonGroup.appendChild(wrapper);
         }
       }
-console.log("10")
+      console.log('10');
       if (buttonGroup.children.length > 0) {
         column.appendChild(buttonGroup);
       }
@@ -89,8 +88,8 @@ console.log("10")
       ul.append(li);
     }
   });
-  console.log("11")
+  console.log('11');
   block.innerHTML = '';
   block.append(ul);
-  console.log("12")
+  console.log('12');
 }
