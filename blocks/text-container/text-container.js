ed2d1912e14e3a5dfbb10021a9bf446326c65723
Venkatalakshmi-nelluri <142808398+Venkatalakshmi-nelluri@users.noreply.github.com>
2025-10-28 19:23:35 +0530

    Text Container issue

--- blocks/text-container/text-container.js
+++ blocks/text-container/text-container.js
@@ -3,9 +3,6 @@ import {} from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   const ul = document.createElement('ul');
-  [...block.children].forEach((row, index) => {
-    
-  });
   console.log('11');
   block.innerHTML = '';
   block.append(ul);
