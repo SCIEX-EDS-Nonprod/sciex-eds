bc783d5dd6943d3bcbe20ff33296c14fc8db5e3a
Venkatalakshmi-nelluri <142808398+Venkatalakshmi-nelluri@users.noreply.github.com>
2025-11-10 11:55:26 +0530

    Resources Grid Block Issue

--- blocks/resources-grid/resources-grid.js
+++ blocks/resources-grid/resources-grid.js
@@ -2,23 +2,5 @@ import {} from '../../scripts/aem.js';
 import {} from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  const rows = [...block.children];
-  if (rows.length === 0) return;
-  const wrapper = document.createElement('div');
-  /* wrapper.classList.add('resources-grid-wrapper');
-  const headingRow = rows[0];
-  if (headingRow) {
-    const headingText = headingRow.querySelector('p')?.textContent || '';
-    if (headingText) {
-      const heading = document.createElement('h3');
-      heading.classList.add('resources-grid-heading');
-      heading.textContent = headingText;
-      wrapper.append(heading);
-    }
-  }
-  const grid = document.createElement('div');
-  grid.classList.add('resources-grid');
-  wrapper.append(grid); */
-  block.textContent = '';
-  block.append(wrapper);
+  console.log(`Decorating resource grid block${block.outerHTML}`);
 }
