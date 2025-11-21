82ad1048909cbfbe865a98c217a5d3b189510795
Venkatalakshmi-nelluri <142808398+Venkatalakshmi-nelluri@users.noreply.github.com>
2025-11-21 18:56:00 +0530

    Atomic Media IMplentation

---
+++ blocks/atomic-media/atomic-media.js
@@ -0,0 +1,23 @@
+import {} from '../../scripts/aem.js';
+import { } from '../../scripts/scripts.js';
+import { } from '../../scripts/dom-builder.js';
+
+export default function decorate(block) {
+  const ul = document.createElement('ul');
+
+  let id = '';
+
+  [...block.children].forEach((row, index) => {
+    if (index === 0) {
+      id = row.textContent.trim();
+      block.id = id;
+      block.parentElement?.classList.add('tabs-container-wrapper');
+    }
+  });
+
+  block.textContent = '';
+
+  // block.parentElement.classList.add('tabs-container-wrapper');
+  // block.append(headingEl);
+  block.append(ul);
+}
