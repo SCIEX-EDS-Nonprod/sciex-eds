a2c66ebbd0f89e684ba41da23e62dcd0f98275ea
sushanth-sciex <sushanth.baddam@sciex.com>
2025-11-25 11:59:48 +0530

    Json changes for container

--- blocks/container/container.js
+++ blocks/container/container.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
-    const container = document.createElement('div');
-    container.className = 'container-block';
-    block.append(accordionContainer);
+  const container = document.createElement('div');
+  container.className = 'container-block';
+  block.append(container);
 }
