7fe89d909ba7c44b2b7a936ac16d1762020ec1a5
sushanth-sciex <sushanth.baddam@sciex.com>
2025-11-25 15:08:31 +0530

    Json changes for container

--- blocks/container/container.js
+++ blocks/container/container.js
@@ -1,5 +1,62 @@
-export default function decorate(block) {
+import { decorateMain } from '../../scripts/scripts.js';
+import { loadSections } from '../../scripts/aem.js';
+
+export async function loadFragment(rawPath) {
+  if (rawPath && rawPath.startsWith('/')) {
+    const cleanPath = rawPath.replace(/(\.plain)?\.html/, '');
+    const resp = await fetch(`${cleanPath}.plain.html`);
+    if (resp.ok) {
+      const main = document.createElement('main');
+      main.innerHTML = await resp.text();
+
+      const resetAttributeBase = (tag, attr) => {
+        main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
+          const absolute = new URL(
+            elem.getAttribute(attr),
+            new URL(cleanPath, window.location),
+          ).href;
+          elem.setAttribute(attr, absolute);
+        });
+      };
+
+      resetAttributeBase('img', 'src');
+      resetAttributeBase('source', 'srcset');
+
+      decorateMain(main);
+      await loadSections(main);
+      return main;
+    }
+  }
+  return null;
+}
+
+export default async function decorate(block) {
+  const columnSetting = Number(block.children[1]?.textContent?.trim());
+  const gridValueColumns = columnSetting > 0 ? columnSetting : 2;
+
+  const links = [...block.querySelectorAll('a')];
+  if (links.length === 0) return;
+
+  block.innerHTML = '';
+
   const container = document.createElement('div');
-  container.className = 'container-block';
-  block.append(container);
+  container.classList.add('fragment-multi-container', `container-grid-${gridValueColumns}`);
+
+  const fragmentPromises = links.map((link) => loadFragment(link.getAttribute('href')));
+
+  const fragments = await Promise.all(fragmentPromises);
+
+  fragments.forEach((fragment) => {
+    if (!fragment) return;
+
+    const fragmentSection = fragment.querySelector(':scope .section');
+    if (fragmentSection) {
+      const wrapper = document.createElement('div');
+      wrapper.classList.add('fragment-item');
+      wrapper.append(...fragmentSection.childNodes);
+      container.appendChild(wrapper);
+    }
+  });
+
+  block.appendChild(container);
 }
