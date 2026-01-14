340083abca13b1df6df5b10affd8158a7d34a133
Anandhan <srinivasu.kondi@sciex.com>
2026-01-14 21:09:27 +0530

    fix

--- blocks/featured-key-workflows/featured-key-workflows.js
+++ blocks/featured-key-workflows/featured-key-workflows.js
@@ -1,4 +1,21 @@
 export default function decorate(block) {
+  const rows = [...block.children];
+  const headingRow = rows[0];
+  const headingText = headingRow.querySelector('p')?.textContent;
    console.log('block',block)
 
+  if (headingText) {
+    const heading = document.createElement('h2');
+    heading.className = 'featured-key-workflows-title';
+    heading.textContent = headingText;
+    block.before(heading);
+  }
+
+  const grid = document.createElement('div');
+  grid.className = 'featured-key-workflows-grid';
+
+ 
+
+  block.innerHTML = '';
+  block.appendChild(grid);
 }
