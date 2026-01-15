d8e8bb94d51135c46e18c3881f60752bcb84c1af
Anandhan <srinivasu.kondi@sciex.com>
2026-01-15 11:25:01 +0530

    fix

--- blocks/featured-key-workflows/featured-key-workflows.js
+++ blocks/featured-key-workflows/featured-key-workflows.js
@@ -3,12 +3,11 @@ export default function decorate(block) {
   const headingRow = rows[0];
   const headingText = headingRow.querySelector('p')?.textContent;
 
-  if (headingText) {
     const heading = document.createElement('h2');
     heading.className = 'featured-key-workflows-title';
     heading.textContent = headingText;
-    block.before(heading);
-  }
+  
 
-  block.textContent = '';
+  block.innerHTML = '';
+  block.append(heading)
 }
