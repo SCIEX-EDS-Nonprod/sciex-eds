f01547cc136c513aa8b330a157cdb29b7c9d3afe
Anandhan <srinivasu.kondi@sciex.com>
2026-02-09 14:44:00 +0530

    fix

--- scripts/searchresult/components/renderSearchResult.js
+++ scripts/searchresult/components/renderSearchResult.js
@@ -8,7 +8,6 @@ import getFavoriteResultsList from '../../favorite-all/favorite-all-controller/f
 const lang = document.documentElement.lang || 'en';
 const strings = i18n[lang] || i18n.en;
 let favoriteResultsList = await getFavoriteResultsList();
-console.log('favoriteResultsList',favoriteResultsList)
 const callFavoriteAPI = async (params) => {
   try {
     const query = new URLSearchParams(params).toString();
@@ -91,10 +90,7 @@ const renderSearchResults = () => {
     sortedResults.forEach((result) => {
       let isFavorite = false
       if (favoriteResultsList.some(fav => fav.pageData.some(page => page.title === result.title))) {
-        console.log('yess',result.title)
         isFavorite = true
-      }else{
-        console.log('nooo', result.title)
       }
       const regulatoryInfo = document.createElement('div');
       regulatoryInfo.className = 'regulatory-info';
