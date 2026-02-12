fdfd1c48d7f4d586a4ee9c55e7c52f479829bd52
sushanth-sciex <sushanth.baddam@sciex.com>
2026-02-12 16:27:37 +0530

    Changes in search result

--- scripts/searchresult/components/renderSearchResult.js
+++ scripts/searchresult/components/renderSearchResult.js
@@ -164,7 +164,9 @@ const renderSearchResults = () => {
 
       const rating = result?.raw?.rating ?? 0;
       Array.from(stars).slice(0, rating).forEach((star) => star.classList.add('filled'));
-      const cleanPrintableUri = getCleanPrintableUri(result.printableUri);
+      const cleanPrintableUri = result.printableUri?.startsWith('https://training.sciex.com')
+      ? getCleanPrintableUri(result.printableUri)
+      : result.printableUri;
 
       const resultItem = document.createElement('div');
       resultItem.className = 'result-item';
