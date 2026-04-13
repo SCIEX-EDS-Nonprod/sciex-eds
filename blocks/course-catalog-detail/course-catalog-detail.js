3d26a3a7ec79af0eee3cb8e1be5d8a60aa91c0f9
Anandhan <srinivasu.kondi@sciex.com>
2026-04-13 09:45:46 +0530

    fix

--- blocks/course-catalog-detail/course-catalog-detail.js
+++ blocks/course-catalog-detail/course-catalog-detail.js
@@ -1,3 +1,17 @@
 export default function decorate(block) {
-  
+  console.log('Decorating course-catalog-detail block', block.children);
+  const children = Array.from(block.children);
+  if (children.length < 2) {
+    return;
+  }
+  const contentEl = children[1];
+
+  // Extract values
+  const content = contentEl?.textContent?.trim();
+
+  const container = document.createElement('div');
+  container.innerHTML = content;
+  block.textContent = '';
+  block.append(container);
+
 }
