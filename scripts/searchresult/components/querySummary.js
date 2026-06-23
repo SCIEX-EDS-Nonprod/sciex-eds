dab730a7a674d653792f30697a29f9de4ac86158
Anandhan <srinivasu.kondi@sciex.com>
2026-06-23 16:11:58 +0530

    jp-search-translation

--- scripts/searchresult/components/querySummary.js
+++ scripts/searchresult/components/querySummary.js
@@ -1,8 +1,7 @@
 import { querySummary } from '../controller/controllers.js';
-import { i18n } from '../../translation.js';
+import { fetchPlaceholders } from '../../aem.js';
 
-const lang = document.documentElement.lang || 'en';
-const strings = i18n[lang] || i18n.en;
+const strings = await fetchPlaceholders();
 
 const renderQuerySummary = () => {
   const querySummaryElement = document.getElementById('query-summary');
@@ -10,9 +9,9 @@ const renderQuerySummary = () => {
   querySummaryElement.innerHTML = '';
   const resultItem = document.createElement('div');
   const querySummaryState = querySummary.state;
-  mobileFilterResultBtn.innerHTML = `${strings.result} (${querySummaryState.total})`;
-  resultItem.innerHTML = `${strings.result} <span> ${querySummaryState.firstResult} -  ${querySummaryState.lastResult} </span>
-                            ${strings.of} <span>${querySummaryState.total}</span>
+  mobileFilterResultBtn.innerHTML = `${strings?.result} (${querySummaryState.total})`;
+  resultItem.innerHTML = `${strings?.result} <span> ${querySummaryState.firstResult} -  ${querySummaryState.lastResult} </span>
+                            ${strings?.of} <span>${querySummaryState.total}</span>
     `;
   querySummaryElement.appendChild(resultItem);
 };
